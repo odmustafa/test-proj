db7d8c33154dcb2867e4126fee00835e41b804f4
odmustafa <odmustafa@gmail.com>
2018-02-25 15:46:38 -0600

    exec firebase init

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = function (width, height) {
-    return width * height;
+var firebase = require("firebase");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyBon8_vJI7kcSvXOfS2v156N3YXYkUoXPQ",
+    authDomain: "test-proj-f5fe2.firebaseapp.com",
+    databaseURL: "https://test-proj-f5fe2.firebaseio.com",
+    projectId: "test-proj-f5fe2",
+    storageBucket: "test-proj-f5fe2.appspot.com"
 };
+firebase.initializeApp(config);
